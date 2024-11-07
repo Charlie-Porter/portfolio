8a3c1bf1c1ab1cf0c9f779dfd48e103b3656cb79
Charles porter <Charles.porter@ioausa.com>
2024-11-07 11:25:59 +0000

    test

--- next.config.js
+++ next.config.js
@@ -5,11 +5,12 @@ const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
   basePath: isProd ? '/react-portfolio' : '',
   assetPrefix: isProd ? '/react-portfolio' : '',
+  output: 'export',
   distDir: 'dist',
   images: {
-    unoptimized: true
+    unoptimized: true, // Required for GitHub Pages with static exports
   },
 };
 
-
 module.exports = nextConfig;
+
