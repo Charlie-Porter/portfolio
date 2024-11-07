04c11a8d670787c14edfc9221f7a203debaf7612
Charles porter <Charles.porter@ioausa.com>
2024-11-07 11:02:45 +0000

    base path

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@
 const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
-  basePath: isProd ? '/react-portfolio' : '',
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
   output: 'export',
   distDir: 'dist',
   images: {
