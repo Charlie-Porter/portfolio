f3759c8c60912d533c39f9f04303821b84b0e2cd
Charles porter <Charles.porter@ioausa.com>
2024-11-11 18:30:13 +0000

    test

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,3 +1,15 @@
 
-export { GET, POST } from "../../../../app/utils/auth"
-export const runtime = "edge" // optional
+import NextAuth from "next-auth";
+import GitHubProvider from "next-auth/providers/github";
+
+const authOptions = {
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    }),
+  ],
+};
+
+export const GET = NextAuth(authOptions);
+export const POST = NextAuth(authOptions);
